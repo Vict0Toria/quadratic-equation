c701d09463682242c4c9ffbb6a3086816b218d8a
Vict0Toria <88_honor@mail.ru>
2026-05-29 16:23:28 +0300

    Update test.cpp

--- src/src/tests/test.cpp
+++ src/src/tests/test.cpp
@@ -1,1 +1,29 @@
+#include <cassert>
+#include <string>
+#include <cmath>
 
+using namespace std;
+
+double solveLinear(double b, double c) {
+    return -c / b;
+}
+
+void testLinear() {
+    double x = solveLinear(2, -4);
+
+    assert(abs(x - 2.0) < 1e-9);
+}
+
+void testDiscriminant() {
+    double D = 2 * 2 - 4 * 1 * 1;
+
+    assert(D == 0);
+}
+
+int main() {
+
+    testLinear();
+    testDiscriminant();
+
+    return 0;
+}
